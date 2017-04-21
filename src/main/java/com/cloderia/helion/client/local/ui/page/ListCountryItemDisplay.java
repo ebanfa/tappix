3c61708ddbe5c928ac590a16936bcb28fddd810e
Edward Banfa <ebanfa@gmail.com>
2017-04-21 23:45:43 +0300

    Tappix Commit

--- src/main/java/com/cloderia/helion/client/local/ui/page/ListCountryItemDisplay.java
+++ src/main/java/com/cloderia/helion/client/local/ui/page/ListCountryItemDisplay.java
@@ -1,97 +1,58 @@
 package com.cloderia.helion.client.local.ui.page;
 
-import java.util.List;
+import static org.jboss.errai.demo.client.local.Click.Type.DOUBLE;
+import static org.jboss.errai.demo.client.local.Click.Type.SINGLE;
 
-import javax.annotation.PostConstruct;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
+import javax.inject.Named;
 
-import org.jboss.errai.bus.client.api.ClientMessageBus;
-import org.jboss.errai.common.client.api.Caller;
-import org.jboss.errai.databinding.client.api.DataBinder;
+import org.jboss.errai.common.client.api.IsElement;
+import org.jboss.errai.common.client.dom.DOMUtil;
+import org.jboss.errai.common.client.dom.Div;
+import org.jboss.errai.common.client.dom.HTMLElement;
+import org.jboss.errai.common.client.dom.Heading;
+import org.jboss.errai.common.client.dom.MouseEvent;
 import org.jboss.errai.databinding.client.components.ListComponent;
-import org.jboss.errai.ui.nav.client.local.DefaultPage;
-import org.jboss.errai.ui.nav.client.local.NavigationPanel;
-import org.jboss.errai.ui.nav.client.local.Page;
-import org.jboss.errai.ui.nav.client.local.PageHiding;
-import org.jboss.errai.ui.nav.client.local.PageShown;
-import org.jboss.errai.ui.shared.api.annotations.AutoBound;
+import org.jboss.errai.demo.client.shared.Country;
+import org.jboss.errai.ui.shared.api.annotations.Bound;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
+import org.jboss.errai.ui.shared.api.annotations.ForEvent;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
-import org.slf4j.Logger;
-
-import com.cloderia.helion.client.local.ui.BasePortalPage;
-import com.cloderia.helion.client.shared.model.Country;
-import com.cloderia.helion.client.shared.operation.CountryOperation;
-import com.cloderia.helion.client.shared.service.CountryStorageService;
 
 /**
  * 
  */
-@Page(path = "/country")
-@Templated(value = "list-country-page.html#app-container")
-public class ListCountryPage extends BasePortalPage {
+@Templated(value = "list-country-page.html#countryListItem")
+public class ListCountryItemDisplay extends BaseCountryView implements IsElement {
+
+  /**
+   * This element is the root element of this component (as declared in the {@code #country} fragment of the
+   * {@link Templated#value()} above).
+   */
+  @Inject
+  @DataField
+  private Div countryListItem;
 
   @Inject
-  @AutoBound
-  private DataBinder<List<Country>> binder;
+  @Bound @DataField
+  private Div name;
+
+  /*
+   * We specify a converter because Errai does not provide built-in conversion from String to Date.
+   */
+  @Inject
+  @Bound(converter = DateConverter.class) @DataField
+  private Div dateCreated;
   
   @Inject
-  @Bound
-  @DataField
-  private ListComponent<Country, CountryItemDisplay> list;
+  @Bound @DataField
+  private Div description;
 
-  /**
-   * This is a simple interface for calling a remote HTTP service. Behind this interface, Errai has generated an HTTP
-   * request to the service defined by {@link CountryStorageService} (a JaxRS service).
-   */
-  @Inject
-  private Caller<CountryStorageService> countryDataService;
-
-  @Inject
-  private ClientMessageBus bus;
-
-  @Inject
-  private Logger logger;
-
-  /**
-   * Register handlers and populate the list of {@link Country Contacts}.
-   */
-  @PostConstruct
-  private void setup() {
-    /*
-     * Triggers an HTTP request to the CountryStorageService. The call back will be invoked asynchronously to display
-     * all retrieved contacts.
-     */
-	//countryService.call((final List<Country> countrys) -> binder.getModel().addAll(countrys)).getAllContacts();
-
-    // Remove placeholder table row from template.
-    //DOMUtil.removeAllElementChildren(list.getElement());
-
-    /*
-     * Configure actions for when a CountryDisplay in the list is selected or deselected.
-     */
-    //list.setSelector(display -> display.setSelected(true));
-    //list.setDeselector(display -> display.setSelected(false));
+  @Override
+  public HTMLElement getElement() {
+    return country;
   }
 
-  /**
-   * This method is invoked when this {@link Page} is attached to the {@link NavigationPanel}.
-   */
-  @PageShown
-  public void addNavBarButtons() {
-  }
-
-  /**
-   * This method is invoked when this {@link Page} is being removed from the {@link NavigationPanel}.
-   */
-  @PageHiding
-  public void removeNavBarButtons() {
-  }
-
-  /**
-   * For ignoring remote events that originate from this client.
-   */
-  private boolean sourceIsNotThisClient(final CountryOperation countryOperation) {
-    return countryOperation.getSourceQueueSessionId() == null || !countryOperation.getSourceQueueSessionId().equals(bus.getSessionId());
-  }
 }
